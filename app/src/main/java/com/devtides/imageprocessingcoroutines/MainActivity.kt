b0e8dc74b777b4854f1c298c2d20b5e2d096b556
danielben <bendaniel20@gmail.com>
2021-03-22 18:45:10 +0100

    Download image and apply filter

--- app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
+++ app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
@@ -1,14 +1,51 @@
 package com.devtides.imageprocessingcoroutines
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO) {
+                getOriginalBitmap()
+            }
+
+            val originalBitmap = originalDeferred.await()
+
+            val filteredDeferred = coroutineScope.async(Dispatchers.Default) { applyFilter(originalBitmap) }
+
+            val filteredBitmap = filteredDeferred.await()
+
+            loadImage(filteredBitmap)
+        }
     }
+
+    private fun getOriginalBitmap(): Bitmap =
+        URL(IMAGE_URL).openStream().use {
+            BitmapFactory.decodeStream(it)
+        }
+
+    private fun loadImage(bmp: Bitmap) {
+        progressBar.isVisible = false
+        imageView.setImageBitmap(bmp)
+        imageView.isVisible = true
+    }
+
+    private fun applyFilter(originalBmp: Bitmap) = Filter.apply(originalBmp)
 }
